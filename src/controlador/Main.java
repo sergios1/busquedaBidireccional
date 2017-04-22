6ca201c79c7ccbbde21360007bf938a0fa3c263f
markoz1 <marco.jhoel@gmail.com>
2017-04-22 19:05:02 -0400

    commit de prueba

--- src/controlador/Main.java
+++ src/controlador/Main.java
@@ -6,6 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("ejecutando main");
 		Ventana ventana = new Ventana();
 		ventana.setVisible(true);
 	}
